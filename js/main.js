c4677c504a53b1dfaf865bdcde60c0f288430a0c
Melson Fernandes <melson_12@hotmail.com>
2017-08-11 21:42:14 +0100

    Layout set up - Sprint1

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Component variables
+//Counter variable for score
+//Counter variable for lives
+
+// ---------------------------------------------\\
+//Identify input box
+//Constructor to create the components 
+// ---------------------------------------------\\
+
+// Logic for when the componets hit the botttom of the screen.
+
+//
+
+// ---------------------------------------------\\
+//Function to update the components.
+
+//Function to clear the componet when correctly inputed
