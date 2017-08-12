645a4e82599a959dd95a5aededc8e7df7c160fd7
Melson Fernandes <melson_12@hotmail.com>
2017-08-12 21:38:15 +0100

    Sprint1 - update 1.0

--- js/main.js
+++ js/main.js
@@ -1,49 +1,34 @@
 $(function(event){
-
+//Counter variable for score
+//Counter variable for lives
+//Component variables
 var $box = $('.box')
-$box.each(function(y,box){
-		
+
+//Identify input box
+$box.each(function(y,box){	
 		$('h5').each(function(index,h5){
 		var boxValue = $(this).text();
 		var eachbox = $box[index];
 		userInput(boxValue,h5,eachbox);
-
-		//console.log(h5);
-
 		})
-
 	})
-
-
-
-
+// ---------------------------------------------\\
 function userInput(bValue,h5,box){
 	$("#usr").keypress(function() {
         if (this.value == bValue) {
             console.log("correct!")
             $(h5).addClass('dissap');
-            $(box).addClass('dissap');
-            
-        }
-
-        
+            $(box).addClass('dissap');   
+        }    
 })
 }
-
-
-
-//Component variables
-//Counter variable for score
-//Counter variable for lives
-
 // ---------------------------------------------\\
-//Identify input box
 //Constructor to create the components 
+
 // ---------------------------------------------\\
 
 // Logic for when the componets hit the botttom of the screen.
 
-//
 
 // ---------------------------------------------\\
 //Function to update the components.
