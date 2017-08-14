07d8dbaceadc7ecd365ee3d3f59a25854892aa35
Melson Fernandes <melson_12@hotmail.com>
2017-08-14 19:19:54 +0100

    Game logic fixed

--- js/main.js
+++ js/main.js
@@ -4,55 +4,84 @@ var player1=0;
 var player2=0;
 var points=0;
 var turn=false;
-// var $p1Turn = $('.player1')
-// var $p2Turn = $('.player2')
-var $scoreplayer2 = $('#p2').text()
-var $scoreplayer1 = $('#p1').text()
+var colors = ["Red","Yellow","Blue","Green","Pink","Purple"];
+var randomtext;
+var coloText = null;
+
+function random(){
+	randomtext=Math.floor(Math.random()*6)
+	switch (randomtext){
+		case 0:coloText=(colors[randomtext]);
+			// console.log(coloText)
+	    	break;
+	    case 1:coloText=(colors[randomtext]);
+	    	// console.log(coloText)
+	    	break;
+	    case 2: coloText=(colors[randomtext]);
+	    	// console.log(coloText)
+	    	break;
+	    case 3:coloText=(colors[randomtext]);
+	    	// console.log(coloText)
+	    	break;
+	    case 4:coloText=(colors[randomtext]);
+	    	// console.log(coloText)
+	    	break;
+	    case 5: coloText=(colors[randomtext]);
+	    	// console.log(coloText)
+	    	break;
+	}
+}
+
+// var $buttAdd = $('.add');
+// 		$buttAdd.click(function(event){
+// 			addDiv();
+// 			})
+	
+function addDiv(){
+	var $div = $('<div class="box"></div>');
+	random();
+	$div.html(coloText);
+	$div.appendTo(".mad");
+	$div.addClass('b1');
+	$div.addClass('box');
+}
 //Counter variable for lives
 //Component variables
-var $box = $('.box')
-//Identify input box
-$box.each(function(y,box){	
-		$('h5').each(function(index,h5){
-		var boxValue = $(this).text();
-		var eachbox = $box[index];
-		userInput(boxValue,h5,eachbox);
-		player2Play(h5,box);
-		restartButton(h5,box);
-		})
-})
 // ---------------------------------------------\\
 // ---------------------------------------------\\
-function userInput(bValue,h5,box){
-	$("#usr").keypress(function() { 
-        if (this.value == bValue) {
-            console.log("correct!")
-            $(h5).addClass('dissap');
-            $(box).addClass('dissap');
-            recordPoints(points+=1); 
-
-        } 
-
+function userInput(){
+	console.log('userInput')
+	$("#usr").keypress(function(event){
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if(keycode == '13'){
+			var userInput = this.value;
+			var $box = $('.box')
+			$box.each(function(index,box){
+				//console.log($(box).html()==="Yellow" || $(box).html()==="Pink" || $(box).html()==="Green")
+				if ($(box).html()===userInput){
+		        $(box).detach();
+		        recordPoints(points+=1);
+						console.log("yes")
+				}
+			})
+	  }
 	})
 }
 // ---------------------------------------------\\
 // Record Points
 function recordPoints(points){
-	//var $score = $('p').text()
-	
 	if (!turn){
-		$('#p1').html(points)	
+		$('#p1').html(points)
 	} else{
 		$('#p2').html(points)
 	}
-
 }
 // ---------------------------------------------\\
 // Player 2 Turn
 function player2Play(h5,box){
 	var $buttonPlay = $('button')
 		$buttonPlay.click(function(){
-			turn = true
+			turn = true;
 			points = 0;
 			$(h5).removeClass('dissap');
 	        $(box).removeClass('dissap');
@@ -85,4 +114,6 @@ function restartButton(h5,box){
 //Function to update the components.
 
 //Function to clear the componet when correctly inputed
+//setInterval(addDiv,2000);
+userInput();
 })
